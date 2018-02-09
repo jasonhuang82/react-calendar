39ff4040a79885153b7155f1c1237685ef69c7ed
jason <jasonhuang702@gmail.com>
2018-02-09 18:19:26 +0800

    first commit

---
+++ preview.js
@@ -0,0 +1,14 @@
+import '../core/core.scss';
+// import js
+import React from 'react';
+import ReactDOM from 'react-dom';
+// 載入es7 環境
+import 'babel-polyfill';
+
+import ReactCalendar from './index.js';
+
+
+ReactDOM.render(
+	<ReactCalendar />,
+	document.getElementById('root')
+);
